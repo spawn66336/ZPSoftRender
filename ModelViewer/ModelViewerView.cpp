99287a785923e60187553f44ef59458f7a30e059
zhangpeng <spawn66336@hotmail.com>
2013-08-14 18:04:31 +0800

    初始化ModelViewer，MFC模型浏览器

---
+++ ModelViewer/ModelViewerView.cpp
@@ -0,0 +1,127 @@
+
+// ModelViewerView.cpp : CModelViewerView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "ModelViewer.h"
+#endif
+
+#include "ModelViewerDoc.h"
+#include "ModelViewerView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CModelViewerView
+
+IMPLEMENT_DYNCREATE(CModelViewerView, CView)
+
+BEGIN_MESSAGE_MAP(CModelViewerView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CModelViewerView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CModelViewerView 构造/析构
+
+CModelViewerView::CModelViewerView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CModelViewerView::~CModelViewerView()
+{
+}
+
+BOOL CModelViewerView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CModelViewerView 绘制
+
+void CModelViewerView::OnDraw(CDC* /*pDC*/)
+{
+	CModelViewerDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CModelViewerView 打印
+
+
+void CModelViewerView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CModelViewerView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CModelViewerView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CModelViewerView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CModelViewerView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CModelViewerView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CModelViewerView 诊断
+
+#ifdef _DEBUG
+void CModelViewerView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CModelViewerView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CModelViewerDoc* CModelViewerView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CModelViewerDoc)));
+	return (CModelViewerDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CModelViewerView 消息处理程序
---
+++ ModelViewer/ModelViewerView.h
@@ -0,0 +1,52 @@
+
+// ModelViewerView.h : CModelViewerView 类的接口
+//
+
+#pragma once
+
+
+class CModelViewerView : public CView
+{
+protected: // 仅从序列化创建
+	CModelViewerView();
+	DECLARE_DYNCREATE(CModelViewerView)
+
+// 特性
+public:
+	CModelViewerDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CModelViewerView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // ModelViewerView.cpp 中的调试版本
+inline CModelViewerDoc* CModelViewerView::GetDocument() const
+   { return reinterpret_cast<CModelViewerDoc*>(m_pDocument); }
+#endif
+
